50f16d1b4052bef8fdb1d137d43ba3a513fd502d
zy455164325 <455164325@qq.com>
2022-11-24 16:36:06 +0800

    2-17 利用mybatis数据库逆向生成工具生成pojo、mapper

--- foodie-dev-api/src/main/java/com/imooc/Application.java
+++ foodie-dev-api/src/main/java/com/imooc/Application.java
@@ -6,11 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 /**
- * App启动类
- */
+ *@ClassName Application
+ *@Description App启动类
+ *@Author braveZeng
+ *@Date 15:18 2022/11/24
+ *Version 1.0
+ **/
 @SpringBootApplication
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
