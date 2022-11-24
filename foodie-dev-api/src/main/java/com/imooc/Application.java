1e9e42b70c55196aad4baaeccc44bf415c3befe0
zy455164325 <455164325@qq.com>
2022-11-24 10:54:15 +0800

    Initial commit

---
+++ foodie-dev-api/src/main/java/com/imooc/Application.java
@@ -0,0 +1,18 @@
+package com.imooc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+/**
+ * App启动类
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
