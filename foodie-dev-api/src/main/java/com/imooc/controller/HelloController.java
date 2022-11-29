cb7849976fafb0222e4dcbada9fd33104b4ada84
zy455164325 <455164325@qq.com>
2022-11-29 18:22:21 +0800

    完成3-12至3-14课程学习，代码提交的主要内容包括：1.用户登录和注册的用户cookie的设置和返回的用户信息的脱敏；2.实现了登录注册后的用户信息的展示；3.移除了spring-boot自带的日志，并整合了log4j日志进行日子记录每日增加生成和记录;

--- foodie-dev-api/src/main/java/com/imooc/controller/HelloController.java
+++ foodie-dev-api/src/main/java/com/imooc/controller/HelloController.java
@@ -1,8 +1,13 @@
 package com.imooc.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.annotations.ApiIgnore;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 
 //@Controller
@@ -11,14 +16,27 @@ import springfox.documentation.annotations.ApiIgnore;
 @RestController
 public class HelloController {
 
-    //final private static Logger logger = LoggerFactory.getLogger(HelloController.class);
+    final private static Logger logger = LoggerFactory.getLogger(HelloController.class);
 
     @GetMapping("/hello")
     public Object hello() {
-
+        logger.info("info: hello");
+        logger.warn("warn: hello");
+        logger.error("error: hello");
         return "Hello World~";
-
     }
+
+    @GetMapping("/setSession")
+    public Object setSession(HttpServletRequest request) {
+        HttpSession session= request.getSession();
+        session.setAttribute("userInfo","new user");
+        session.setMaxInactiveInterval(3600); //设置会话超时时间
+        session.getAttribute("userInfo"); //获取session中的key值
+//        session.removeAttribute("userInfo"); //移除设置的key信息
+        return "ok";
+    }
+
+
 
 
 }
